3912b0fa41e0d28f204f41a34b152338ec682760
DDC <diaz_carrete@yahoo.com>
2014-10-12 14:27:17 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+colchis
+=======
+
+Colchis wants to be a rudimentary client for JSON-RPC 2.0 over plain TCP sockets.
---
+++ src/Network/Colchis.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE ExistentialQuantification #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ViewPatterns #-}
+
+module Network.Colchis ()  where
+
+
+
+
+
+
