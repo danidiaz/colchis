2487f80a15194595fa209e6bb982fe33bd57fb22
DDC <diaz_carrete@yahoo.com>
2014-10-16 18:21:26 -0400

    working on adapt

--- src/Network/Colchis/Adapter/JSONRPC20.hs
+++ src/Network/Colchis/Adapter/JSONRPC20.hs
@@ -1,13 +1,18 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE ViewPatterns #-}
+{-# LANGUAGE OverloadedStrings #-}
+
 module Network.Colchis.Adapter.JSONRPC20 (
        module Network.Colchis.Adapter
     ) where
 
 import Network.Colchis.Adapter
-import qualified Network.Colchis.Adapter.JSONRPC20.Request as IN
-import qualified Network.Colchis.Adapter.JSONRPC20.Response as OUT 
+import qualified Network.Colchis.Adapter.JSONRPC20.Request as OUT
+import qualified Network.Colchis.Adapter.JSONRPC20.Response as IN 
 
 import Data.Text
 import Data.Aeson
+import Data.Aeson.Types
 import Control.Monad
 import Control.Monad.Trans.Except
 import Control.Monad.Trans.State.Strict
@@ -25,6 +30,26 @@ adaptToJSONRPC20 = evalStateP 0 `liftM` go
   where
     go (method,j) = do 
         msgId <- freshId                                
-        undefined 
+        let req = OUT.Request protocolVer
+                              method
+                              j
+                              msgId
+        jresp <- request $ toJSON req
+        case parseEither parseJSON jresp of
+            Left str -> throwE' ()
+            Right (IN.Response p' rm' em' id') -> do
+                if protocolVer /= p' 
+                    then throwE' ()
+                    else do
+                        if msgId /= id'
+                            then throwE' ()
+                            else case em' of 
+                                Just err -> throwE' ()
+                                Nothing -> case rm' of
+                                   Nothing -> throwE' ()
+                                   Just val -> respond val >>= go 
     freshId = lift $ withStateT (flip mod 100 . succ) get
+    protocolVer = "2.0"
+    throwE' = lift . lift . throwE
+
 
